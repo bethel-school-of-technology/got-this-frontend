2292c5860dd5e71b6c34bc44ff10906a70deba1a
ProjectMaster98 <sourstrawberry822@gmail.com>
2019-12-19 23:14:47 -0500

    adding the calendar basics

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,24 +3,21 @@ import { Routes, RouterModule } from '@angular/router';
 import { CounterListComponent } from './counter-list/counter-list.component';
 import { CounterAddComponent } from './counter-add/counter-add.component';
 import { CounterEditComponent } from './counter-edit/counter-edit.component';
+import { AboutComponent } from './about/about.component';
 import { CalendarComponent } from './calendar/calendar.component';
-import {AboutComponent} from './about/about.component';
 
 const routes: Routes = [
-  {path: "", redirectTo: "about", pathMatch:"full"},
-  
-    {path:"list", component: CounterListComponent},
-     {path:"add", component:CounterAddComponent},
+  { path: "", redirectTo: "about", pathMatch: "full" },
   {path: "calendar", component: CalendarComponent},
-  {path: "about", component: AboutComponent},
-    {path: "edit/:id", component: CounterEditComponent},
-   
-]
+  { path: "list", component: CounterListComponent },
+  { path: "add", component: CounterAddComponent },
+  { path: "about", component: AboutComponent },
+  { path: "edit/:id", component: CounterEditComponent },
 
-;
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule {}
+export class AppRoutingModule { }
--- src/app/calendar/calendar.component.ts
+++ src/app/calendar/calendar.component.ts
@@ -1,6 +1,42 @@
 import { Component, OnInit } from '@angular/core';
-import { CountDataService} from '../count-data.service';
+import { Counter } from '../models/counter';
+import {CountDataService} from '../services/count-data.service'
 import { Router } from '@angular/router';
+
+import {
+  startOfDay,
+  endOfDay,
+  subDays,
+  addDays,
+  endOfMonth,
+  isSameDay,
+  isSameMonth,
+  addHours
+} from 'date-fns';
+
+import {
+  CalendarEvent,
+  CalendarEventAction,
+  CalendarEventTimesChangedEvent,
+  CalendarView
+} from 'angular-calendar';
+
+import { Subject } from 'rxjs';
+
+const colors: any = {
+    red: {
+      primary: '#ad2121',
+      secondary: '#FAE3E3'
+    },
+    blue: {
+      primary: '#1e90ff',
+      secondary: '#D1E8FF'
+    },
+    yellow: {
+      primary: '#e3bc08',
+      secondary: '#FDF1BA'
+    }
+  };
 
 @Component({
   selector: 'app-calendar',
@@ -9,10 +45,59 @@ import { Router } from '@angular/router';
 })
 export class CalendarComponent implements OnInit {
 
-  
+  CalendarView = CalendarView;
+
+  viewDate: Date = new Date();
+
+
+  events: CalendarEvent[] = [
+    {
+      start: subDays(startOfDay(new Date()), 1),
+      end: addDays(new Date(), 1),
+      title: 'A 3 day event',
+      color: colors.red,
+      actions: [],
+      allDay: true,
+      resizable: {
+        beforeStart: true,
+        afterEnd: true
+      },
+      draggable: true
+    },
+    {
+      start: startOfDay(new Date()),
+      title: 'An event with no end date',
+      color: colors.yellow,
+      actions: []
+    },
+    {
+      start: subDays(endOfMonth(new Date()), 3),
+      end: addDays(endOfMonth(new Date()), 3),
+      title: 'A long event that spans 2 months',
+      color: colors.blue,
+      allDay: true
+    },
+    {
+      start: addHours(startOfDay(new Date()), 2),
+      end: addHours(new Date(), 2),
+      title: 'A draggable and resizable event',
+      color: colors.yellow,
+      actions: [],
+      resizable: {
+        beforeStart: true,
+        afterEnd: true
+      },
+      draggable: true
+    }
+  ];
+  activeDayIsOpen: boolean = true;
+
+  refresh: Subject<any> = new Subject();
+
   constructor(private counterDataService: CountDataService, private router: Router) { }
 
   ngOnInit() {
   }
 
 }
+
